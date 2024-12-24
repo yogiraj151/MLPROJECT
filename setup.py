b3f5b424da1a5384630b2395c4a1c5d1fe67d357
yogiraj151 <chaukhandeyogiraj9@gmail.com>
2024-12-24 12:15:38 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+        
+    return requirements
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='yogiraj',
+    author_email='chaukhandeyogiraj9@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
