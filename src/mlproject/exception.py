9c661b74f36fe55620958a1b6bc0ae59b75b7b8e
yogiraj151 <chaukhandeyogiraj9@gmail.com>
2024-12-29 11:56:01 +0530

    data transformation

--- src/mlproject/exception.py
+++ src/mlproject/exception.py
@@ -9,11 +9,11 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+
 class CustomException(Exception):
     def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_details)
-        
+
     def __str__(self):
         return self.error_message
-        
